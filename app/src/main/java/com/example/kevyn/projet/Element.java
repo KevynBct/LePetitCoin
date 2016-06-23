4ae5e2f2b9ac580f4ba78c5bec8ef1fe11fa4087
Wahey <kevyn.buchet@gmail.com>
2016-06-23 22:38:02 +0200

    23/06/2016 - 22:37

--- app/src/main/java/com/example/kevyn/projet/Element.java
+++ app/src/main/java/com/example/kevyn/projet/Element.java
@@ -6,13 +6,14 @@ package com.example.kevyn.projet;
 public class Element {
     private String commune;
     private String adresse;
-    private String position;
+    private Double lat;
+    private Double lng;
     private String horaires;
 
     public Element(String _commune, String _adresse, String _position, String _horaires){
         this.commune = _commune;
         this.adresse = _adresse;
-        this.position = _position;
+        this.toLngLat(_position);
         this.horaires = _horaires;
     }
 
@@ -24,15 +25,26 @@ public class Element {
         return this.adresse;
     }
 
-    public String getPosition(){
-        return this.position;
-    }
+    public Double getLat(){ return this.lat; }
+
+    public Double getLng(){ return this.lng; }
 
     public String getHoraires(){
         return this.horaires;
     }
 
+    public void toLngLat(String _position){
+        String [] tab;
+        tab = _position.split(",");
+
+        tab[0] = tab[0].substring(1);
+        tab[1] = tab[1].substring(0, tab[1].length()-1);
+
+        this.lat = Double.parseDouble(tab[0]);
+        this.lng = Double.parseDouble(tab[1]);
+    }
+
     public String toString(){
-        return this.adresse+", "+this.commune+", "+this.position+", "+this.horaires;
+        return this.adresse+", "+this.commune+", "+this.horaires;
     }
 }
--- app/src/main/java/com/example/kevyn/projet/MainActivity.java
+++ app/src/main/java/com/example/kevyn/projet/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.kevyn.projet;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -18,6 +20,7 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.ui.PlacePicker;
 
 import org.json.JSONArray;
@@ -28,7 +31,7 @@ import org.json.JSONTokener;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    private ArrayList<Element> resultListe;
+    private ArrayList<Element> resultList;
     private AdapterElement adapter;
     private ListView list;
     private Spinner spinner;
@@ -38,11 +41,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        resultListe = new ArrayList<Element>();
-        adapter = new AdapterElement(this, resultListe);
+        // ResultList
+        resultList = new ArrayList<Element>();
+        adapter = new AdapterElement(this, resultList);
         list = (ListView) findViewById(R.id.resultList);
         list.setAdapter(adapter);
 
+        // Spinner
         spinner = (Spinner) findViewById(R.id.spinner);
         ArrayList<Integer> spinnerList = new ArrayList<Integer>();
         spinnerList.add(5);
@@ -51,27 +56,53 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<Integer> dataAdapter = new ArrayAdapter<Integer>(this,android.R.layout.simple_spinner_item, spinnerList);
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(dataAdapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                doRequest(createAdresseRequest());
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {}
+        });
 
     }
 
     public void searchButtonClick(View v){
-        EditText    editText = (EditText) findViewById(R.id.searchBar);
-        String      adresseRequest = "http://data.nantes.fr/api/publication/24440040400129_NM_NM_00170/Toilettes_publiques_nm_STBL/content?filter={\"COMMUNE\":{\"$eq\":\""+editText.getText()+"\"}}";
-
-        this.doRequest(adresseRequest);
-
+        this.doRequest(this.createAdresseRequest());
         adapter.notifyDataSetChanged();
     }
 
     public void mapButtonClick(View v) throws GooglePlayServicesNotAvailableException, GooglePlayServicesRepairableException {
         int PLACE_PICKER_REQUEST = 1;
         PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
-
         startActivityForResult(builder.build(this), PLACE_PICKER_REQUEST);
     }
 
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlacePicker.getPlace(data, this);
+                String toastMsg = String.format("Place: %s", place.getName());
+                Toast.makeText(this, toastMsg, Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    public String createAdresseRequest(){
+        EditText    editText = (EditText) findViewById(R.id.searchBar);
+        String result = "http://data.nantes.fr/api/publication/24440040400129_NM_NM_00170/Toilettes_publiques_nm_STBL/content";
+
+        if(editText.getText().length() != 0) result += "?filter={\"COMMUNE\":{\"$eq\":\""+editText.getText()+"\"}}";
+
+
+        return result;
+    }
+
+
     public void doRequest(String _adresseRequest){
-        resultListe.clear();
+        resultList.clear();
         RequestQueue queue = Volley.newRequestQueue(this);
         StringRequest stringRequest =
                 new StringRequest(
@@ -83,7 +114,8 @@ public class MainActivity extends AppCompatActivity {
                                     JSONArray jsonArray = repObj.optJSONArray("data");
                                     for(int i=0; i < jsonArray.length(); i++){
                                         JSONObject jsonObject = jsonArray.getJSONObject(i);
-                                        resultListe.add(new Element(jsonObject.optString("COMMUNE").toString(),jsonObject.optString("ADRESSE").toString(),jsonObject.optString("_l").toString(), jsonObject.optString("INFOS_HORAIRES").toString()));
+                                        if(resultList.size() < (Integer) spinner.getSelectedItem())
+                                        resultList.add(new Element(jsonObject.optString("COMMUNE").toString(),jsonObject.optString("ADRESSE").toString(),jsonObject.optString("_l").toString(), jsonObject.optString("INFOS_HORAIRES").toString()));
                                     }
                                 } catch (JSONException je) {
                                     Toast.makeText(getApplicationContext(), je.toString(), Toast.LENGTH_SHORT).show();
